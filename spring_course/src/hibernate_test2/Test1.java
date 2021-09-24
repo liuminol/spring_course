e02c9f05c06bb01c01ad2702a48f2d27a4d4054d
liuminol <aplastyr@yandex.ru>
2021-09-24 14:16:11 +0400

    introduce one to one uni-directional relation

---
+++ spring_course/src/hibernate_test2/Test1.java
@@ -0,0 +1,58 @@
+package hibernate_test2;
+
+import hibernate_test2.entity.Detail;
+import hibernate_test2.entity.Employee;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+public class Test1 {
+    public static void main(String[] args) {
+        SessionFactory factory = new Configuration()
+                .configure("hibernate.cfg.xml")
+                .addAnnotatedClass(Employee.class)
+                .addAnnotatedClass(Detail.class)
+                .buildSessionFactory();
+
+        Session session = null;
+
+        try {
+//            Session session = factory.getCurrentSession();
+//            Employee employee = new Employee("Name", "Surname", "IT", 800);
+//            Detail detail = new Detail("City", "456465", "mail");
+//
+//            employee.setEmpDetail(detail);
+//            session.beginTransaction();
+//
+//            session.save(employee);
+//
+//            session.getTransaction().commit();
+//            System.out.println("Done");
+
+//            Session session = factory.getCurrentSession();
+//            Employee employee = new Employee("Name2", "Surname2", "Sales", 700);
+//            Detail detail = new Detail("City2", "40565", "gmail");
+//
+//            employee.setEmpDetail(detail);
+//            session.beginTransaction();
+//
+//            session.save(employee);
+//
+//            session.getTransaction().commit();
+//            System.out.println("Done");
+
+            session = factory.getCurrentSession();
+            session.beginTransaction();
+
+            Employee emp = session.get(Employee.class, 2);
+            session.delete(emp);
+
+            session.getTransaction().commit();
+            System.out.println("Done");
+
+        } finally {
+            session.close();
+            factory.close();
+        }
+    }
+}
---
+++ spring_course/src/hibernate_test2/entity/Detail.java
@@ -0,0 +1,73 @@
+package hibernate_test2.entity;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "details")
+public class Detail {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private int id;
+
+    @Column(name = "city")
+    private String city;
+
+    @Column(name = "phone_number")
+    private String phoneNumber;
+
+    @Column(name = "email")
+    private String email;
+
+    public Detail() {
+    }
+
+    public Detail(String city, String phoneNumber, String email) {
+        this.city = city;
+        this.phoneNumber = phoneNumber;
+        this.email = email;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @Override
+    public String toString() {
+        return "Detail{" +
+                "id=" + id +
+                ", city='" + city + '\'' +
+                ", phoneNumber='" + phoneNumber + '\'' +
+                ", email=" + email +
+                '}';
+    }
+}
